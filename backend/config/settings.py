51a6cd451cb347721a74aac361973cbf6fc9e369
maran <sugumaran.rc.0107@gmail.com>
2026-02-11 22:34:10 +0530

    update production security settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,9 +57,13 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    'https://react-django-crud.onrender.com',
 ]
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com','https://react-django-crud.onrender.com']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://react-django-crud.onrender.com',
+]
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
